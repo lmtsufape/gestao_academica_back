0071fb3fb309afc260f92730a39e2c6997c63a97
Caio <caio.vsg@hotmail.com>
2024-09-11 18:41:30 -0300

    feat: Classes de DTO

---
+++ src/main/java/br/edu/ufape/sgi/comunicacao/validacoes/NumeroValidoValidator.java
@@ -0,0 +1,22 @@
+package br.edu.ufape.sgi.comunicacao.validacoes;
+
+import br.edu.ufape.sgi.comunicacao.annotations.NumeroValido;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class NumeroValidoValidator implements ConstraintValidator<NumeroValido, String> {
+
+    // Expressão regular para validar o formato (XX)XXXXX-XXXX
+    private static final String PHONE_NUMBER_PATTERN = "^\\(\\d{2}\\) \\d{5}-\\d{4}$";
+
+
+    @Override
+    public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
+        if (phoneNumber == null || phoneNumber.isBlank()) {
+            return false; // Número de telefone não pode ser nulo ou vazio
+        }
+        // Verifica se o número de telefone corresponde ao padrão
+        return phoneNumber.matches(PHONE_NUMBER_PATTERN);
+    }
+
+}
