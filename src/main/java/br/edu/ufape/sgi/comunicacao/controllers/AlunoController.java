8bced54dea660d25b06fc12d561c9178c7bc01ff
Caio <caio.vsg@hotmail.com>
2024-10-01 17:13:13 -0300

    fix: remoção de métodos depreciados no controller de aluno

--- src/main/java/br/edu/ufape/sgi/comunicacao/controllers/AlunoController.java
+++ src/main/java/br/edu/ufape/sgi/comunicacao/controllers/AlunoController.java
@@ -1,12 +1,10 @@
 package br.edu.ufape.sgi.comunicacao.controllers;
 
-import br.edu.ufape.sgi.comunicacao.dto.aluno.AlunoPatchRequest;
-import br.edu.ufape.sgi.comunicacao.dto.aluno.AlunoRequest;
+
 import br.edu.ufape.sgi.comunicacao.dto.aluno.AlunoResponse;
 import br.edu.ufape.sgi.exceptions.aluno.AlunoNotFoundException;
 import br.edu.ufape.sgi.fachada.Fachada;
 import br.edu.ufape.sgi.models.Aluno;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
@@ -20,12 +18,6 @@ public class AlunoController {
     private final Fachada fachada;
     private final ModelMapper modelMapper;
 
-    @PostMapping
-    public ResponseEntity<AlunoResponse> salvar(@Valid @RequestBody AlunoRequest aluno) {
-       Aluno response = fachada.salvar(aluno.convertToEntity(aluno, modelMapper));
-       return new ResponseEntity<>(new AlunoResponse(response, modelMapper), HttpStatus.CREATED);
-    }
-
     @GetMapping("/{id}") ResponseEntity<AlunoResponse> buscarAluno(@PathVariable Long id) throws AlunoNotFoundException {
         Aluno response = fachada.buscarAluno(id);
         return new ResponseEntity<>(new AlunoResponse(response, modelMapper), HttpStatus.OK);
@@ -33,13 +25,6 @@ public class AlunoController {
 
     @GetMapping List<AlunoResponse> listarAlunos() {
         return fachada.listarAlunos().stream().map(aluno -> new AlunoResponse(aluno, modelMapper)).toList();
-    }
-
-    @PatchMapping("/{id}")
-    ResponseEntity<AlunoResponse> atualizarAluno(@PathVariable Long id, @Valid @RequestBody AlunoPatchRequest entity) throws AlunoNotFoundException{
-        Aluno aluno = fachada.buscarAluno(id);
-        modelMapper.map(entity, aluno);
-        return new ResponseEntity<>(new AlunoResponse(fachada.salvar(aluno), modelMapper), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
