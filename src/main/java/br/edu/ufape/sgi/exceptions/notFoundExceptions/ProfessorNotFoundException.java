665ee0c69de60afcb49fd7f753ca943e5782ee16
Caio <caio.vsg@hotmail.com>
2024-10-24 18:39:54 -0300

    refactor: mensagem incorreta na exception

--- src/main/java/br/edu/ufape/sgi/exceptions/notFoundExceptions/ProfessorNotFoundException.java
+++ src/main/java/br/edu/ufape/sgi/exceptions/notFoundExceptions/ProfessorNotFoundException.java
@@ -3,6 +3,6 @@ package br.edu.ufape.sgi.exceptions.notFoundExceptions;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = org.springframework.http.HttpStatus.NOT_FOUND, reason = "Professor não encontrado, verifique se o usuário é um aluno")
+@ResponseStatus(value = org.springframework.http.HttpStatus.NOT_FOUND, reason = "Professor não encontrado, verifique se o usuário é um professor")
 public class ProfessorNotFoundException extends ChangeSetPersister.NotFoundException {
 }
